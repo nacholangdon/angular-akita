3ad7bf8d2534495411253a291230819eaa9df46e
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-20 23:11:15 -0300

    Add main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SetNameComponent } from './set-name/set-name.component';
+import { GetNameComponent } from './get-name/get-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetNameComponent,
+    GetNameComponent
   ],
   imports: [
     BrowserModule
