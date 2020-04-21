b0c20239e0796df9eb2c2fef9ef05c622c354176
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-20 23:29:58 -0300

    Finish sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 import { AppComponent } from './app.component';
 import { SetNameComponent } from './set-name/set-name.component';
 import { GetNameComponent } from './get-name/get-name.component';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { GetNameComponent } from './get-name/get-name.component';
     GetNameComponent
   ],
   imports: [
-    BrowserModule
+    environment.production ? [] : AkitaNgDevtools,
+    CommonModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
